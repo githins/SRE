b9ceda806b51467b5161fd1cc4e30602c2e50fff
githins <githin.shaji@my.jcu.edu.au>
2018-06-01 13:05:07 +1000

    Models

---
+++ services/profile.ts
@@ -0,0 +1,14 @@
+import { EmailValidator } from "@angular/forms/src/directives/validators";
+
+export interface Profile{
+
+    username: string;
+    firstName: string;
+    lastName: string;
+    mentorName: string;
+    Date:Date;
+    Email: string;
+    Phone: number;
+
+
+}
